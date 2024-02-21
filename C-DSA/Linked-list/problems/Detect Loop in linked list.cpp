9e6ac6d490d1956827915d75c629b2c8bc34befe
HARSHVARDHAN BARETH <137777618+HARSHVARDHAN-BARETH@users.noreply.github.com>
2024-02-21 17:38:57 +0530

    Add files via upload

---
+++ C-DSA/Linked-list/problems/Detect Loop in linked list.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+    public:
+    //Function to check if the linked list has a loop.
+    bool detectLoop(Node* head)
+    {
+          Node *slow = head;
+          Node *fast = head;
+          while(slow && fast && fast->next)
+          {
+              slow = slow->next;
+              fast = fast->next->next;
+              if(slow==fast)
+              {
+                  return 1;
+              }
+
+          }
+        return 0;
+
+    }
+};
