0529aac10e1d8daaa9b41bb66368a8739c968ee4
HARSHVARDHAN BARETH <137777618+HARSHVARDHAN-BARETH@users.noreply.github.com>
2024-02-23 17:40:04 +0530

    Add files via upload

---
+++ C-DSA/Linked-list/problems/24. Swap Nodes in Pairs.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+        ListNode* first = head;
+        ListNode* dummy = new ListNode(0);
+        ListNode* prev = dummy;
+        prev -> next = head;
+        
+        while(first && first->next)
+        {
+            ListNode* second = first->next;
+            ListNode* future = first->next->next;
+            
+            second->next = first;
+            prev->next = second;
+            first->next = future;
+            prev = first;
+            first = future;
+        }
+        
+        return dummy->next;
+    }       
+};
