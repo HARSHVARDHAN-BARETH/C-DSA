98ec564450588815724e3727b004b7abbec47c7c
HARSHVARDHAN BARETH <137777618+HARSHVARDHAN-BARETH@users.noreply.github.com>
2024-04-13 19:01:09 +0530

    Update Trees Implemetaion.cpp

--- C-DSA/Trees/Trees Implemetaion.cpp
+++ C-DSA/Trees/Trees Implemetaion.cpp
@@ -11,37 +11,39 @@ class node
     
     node(int d)
     {
-        this->data = d;
-        this->left = NULL;
-        this->right = NULL;
+        this -> data = d;
+        this -> left = NULL;
+        this -> right = NULL;
     }
 };
 
-node* BuildTree(node* root)
+
+node* buildTree(node* root)
 {
-    cout << "Enter the data : " << endl;
+    cout << "Enter the data " << endl;
     int data;
     cin >> data;
-    root  = new node(data);
+    root = new node(data);
     
     if(data == -1)
     {
         return NULL;
     }
     
-    cout << "Inserting data for left : " << endl;
-    root->left = BuildTree(root->left);
-    cout << "Inserting data for right : " << endl;
-    root->right = BuildTree(root->right);
+    cout << "Enter data for inserting in left " << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in rigth side " << endl;
+    root->right = buildTree(root->right);
     
     return root;
+    
 }
 
 int main()
 {
     node* root = NULL;
-
-    root = BuildTree(root);
+    
+    root = buildTree(root);
 
     return 0;
 }
